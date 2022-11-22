02a37ee3e31bfaee745b0cf5ff33d56e914c62f7
SomEnaMeforme <98802409+SomEnaMeforme@users.noreply.github.com>
2022-11-22 21:08:00 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+<<<<<<< Updated upstream
 import {createMockyData} from "./mocky-data.js";
+=======
+import {getPhotos} from './mock.js';
+import {initThumbnails} from './thumbnails.js';
+
+const mockyData = getPhotos();
+initThumbnails(mockyData);
+>>>>>>> Stashed changes
 
 const mockyData = createMockyData(15);
